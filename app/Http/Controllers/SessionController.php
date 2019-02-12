6f3a9980f474defbb3e62e86643c132d807cbca0
zmg02 <1225798710@qq.com>
2019-02-12 08:21:23 +0000

    Add account activations:

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -19,20 +19,26 @@ class SessionController extends Controller
     	return view('sessions.create');
     }
 
-    public function store(Request $request)
+     public function store(Request $request)
     {
-    	$credentials = $this->validate($request, [
-    		'email' => 'required|email|max:255',
-    		'password' => 'required'
-    	]);
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
 
-    	if (Auth::attempt($credentials, $request->has('remember'))) {
-    		session()->flash('success', '欢迎回来！');
-    		return redirect()->intended(route('users.show', [Auth::user()]));
-    	} else {
-    		session()->flash('danger', '佷抱歉，您的邮箱和密码不匹配');
-    		return redirect()->back();
-    	}
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           if(Auth::user()->activated) {
+               session()->flash('success', '欢迎回来！');
+               return redirect()->intended(route('users.show', [Auth::user()]));
+           } else {
+               Auth::logout();
+               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+           }
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
+       }
     }
 
     public function destroy()
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user->email = '1096173392@qq.com';
         $user->password = bcrypt('password');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }

6a9cfada770f4fc9805fdbaee1205a7751c65010
zmg02 <1225798710@qq.com>
2019-02-25 02:55:46 +0000

    关于页修改

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -2,5 +2,13 @@
 
 @section('title', '关于')
 @section('content')
-<h1>关于页</h1>
+<div class="col-md-12">
+	<h1>zmg个人博客</h1>
+	<p class="explain">
+		<h3>说明：</h3>
+		博客内容有：技术问题、个人感悟、鸡汤文等；<br/>
+		后续改进：发图、评论功能；<br/>
+		谢谢关注！<br/>
+	</p>
+</div>
 @stop
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -23,7 +23,7 @@
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training/5.5">Laravel 入门教程</a> 的示例项目主页。
+        你现在所看到的是 <a href="{{ route('about') }}">Laravel 个人博客</a> 的示例项目主页。
       </p>
       <p>
         一切，将从这里开始。
